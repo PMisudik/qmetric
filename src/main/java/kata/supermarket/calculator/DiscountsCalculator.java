1407e56fc72be360120570da9e491e757c629287
Pavol Misudik <misudikpavol1@gmail.com>
2020-10-23 12:55:14 +0200

    Add three for two discount

--- src/main/java/kata/supermarket/calculator/DiscountsCalculator.java
+++ src/main/java/kata/supermarket/calculator/DiscountsCalculator.java
@@ -24,16 +24,4 @@ public class DiscountsCalculator implements Calculator {
                 .setScale(2, RoundingMode.HALF_UP);
     }
 
-    /**
-     * TODO: This could be a good place to apply the results of
-     * the discount calculations.
-     * It is not likely to be the best place to do those calculations.
-     * Think about how Basket could interact with something
-     * which provides that functionality.
-     */
-    private BigDecimal discounts() {
-        return BigDecimal.ZERO;
-    }
-
-
 }
